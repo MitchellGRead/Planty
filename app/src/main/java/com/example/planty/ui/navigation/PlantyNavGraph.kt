65996c9ce14ae44a950ea8868eb9bdfec3a89a76
MitchellGRead <mitchell.g.read@gmail.com>
2021-10-12 07:26:16 -0700

    Initial navgraph setup

--- app/src/main/java/com/example/planty/ui/navigation/PlantyNavGraph.kt
+++ app/src/main/java/com/example/planty/ui/navigation/PlantyNavGraph.kt
@@ -1,16 +1,20 @@
 package com.example.planty.ui.navigation
 
-import androidx.compose.material.ScaffoldState
-import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.planty.ui.home.HomeScreen
 
 @Composable
 fun PlantyNavGraph(
-    navController: NavController = rememberNavController(),
-    scaffoldState: ScaffoldState = rememberScaffoldState(),
+    navController: NavHostController = rememberNavController(),
     startDestination: String = PlantyDestinations.HOME_SCREEN_ROUTE
 ) {
-//    TODO - setup nav graph
+    NavHost(navController = navController, startDestination = startDestination) {
+        composable(PlantyDestinations.HOME_SCREEN_ROUTE) {
+            HomeScreen(name = "Mitchell")
+        }
+    }
 }
