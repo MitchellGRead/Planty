2db1593799e0ccc82d938d3779d89326b7325cae
MitchellGRead <mitchell.g.read@gmail.com>
2021-11-10 07:40:56 -0800

    preliminary work for bottom nav bar

--- app/src/main/java/com/example/planty/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/planty/ui/home/HomeScreen.kt
@@ -1,15 +1,23 @@
 package com.example.planty.ui.home
 
 import android.content.res.Configuration
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.planty.R
 import com.example.planty.domain.model.PlantEntry
 import com.example.planty.ui.common.composables.InsetAwareTopAppBar
+import com.example.planty.ui.navigation.PlantyDestinations
 import com.example.planty.ui.theme.Dimen
 import com.example.planty.ui.theme.PlantyTheme
+import timber.log.Timber
 
 @Composable
 fun HomeScreen(
@@ -17,10 +25,15 @@ fun HomeScreen(
 ) {
     val card = PlantEntry(id = "1", name = "Planty")
     val cards: List<PlantEntry> = listOf(card, card, card, card,card, card, card, card,card, card, card, card)
+    val currentBottomNavDest = remember { mutableStateOf(PlantyDestinations.HOME_SCREEN_ROUTE) }
 
     Scaffold(
         scaffoldState = scaffoldState,
-        topBar = { HomeScreenTopBar() }
+        topBar = { HomeScreenTopBar() },
+        floatingActionButton = { HomeScreenFAB() },
+        isFloatingActionButtonDocked = true,
+        floatingActionButtonPosition = FabPosition.Center,
+        bottomBar = { HomeScreenBottomBar(currentBottomNavDest.value) }
     ) {
         PlantCardGrid(cards = cards)
     }
@@ -33,6 +46,52 @@ fun HomeScreenTopBar() {
         backgroundColor = MaterialTheme.colors.surface,
         elevation = Dimen.Zero
     )
+}
+
+@Composable
+fun HomeScreenFAB() {
+    FloatingActionButton(
+        onClick = { Timber.d("FAB Clicked") },
+        shape = CircleShape
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Add,
+            contentDescription = stringResource(R.string.AddPlantEntry)
+        )
+    }
+}
+
+@Composable
+fun HomeScreenBottomBar(currentDest: String) {
+    BottomAppBar(
+        cutoutShape = CircleShape
+    ) {
+        BottomNavigation {
+            BottomNavigationItem(
+                selected = currentDest == PlantyDestinations.HOME_SCREEN_ROUTE,
+                onClick = { Timber.d("I was clicked") },
+                icon = {
+                    Icon(
+                        imageVector = Icons.Filled.Home,
+                        contentDescription = stringResource(R.string.Home)
+                    )
+                },
+                label = { Text(text = stringResource(id = R.string.Home)) }
+            )
+
+            BottomNavigationItem(
+                selected = currentDest == PlantyDestinations.HOME_SCREEN_ROUTE,
+                onClick = { Timber.d("I was clicked") },
+                icon = {
+                    Icon(
+                        imageVector = Icons.Filled.Home,
+                        contentDescription = stringResource(R.string.Home)
+                    )
+                },
+                label = { Text(text = stringResource(id = R.string.Home)) }
+            )
+        }
+    }
 }
 
 @Preview(name = "default")
